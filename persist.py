3812184b4f76a618de6a773dd36aa70773e6c420
Emotions-Gaming <Studio@emotions-gaming.de>
2025-06-22 23:35:23 +0200

    da

--- persist.py
+++ persist.py
@@ -8,6 +8,7 @@ from utils import is_admin  # <-- nutzt Rechte-Check aus utils.py
 from utils import load_json, save_json
 from permissions import has_permission_for
 
+
 # === Konfiguration ===
 
 PERSIST_DIR = "persistent_data"
